f44956eb4ae28425abd5fd646cfeaa3eba29b961
convoluted <duminydaniel@gmail.com>
2018-02-28 23:03:03 +0530

    Update idea.php

--- idea.php
+++ idea.php
@@ -16,8 +16,8 @@ body {font-family: "Lato", sans-serif}
 <div class="w3-top">
   <div class="w3-bar w3-black w3-card">
     <a class="w3-bar-item w3-button w3-padding-large w3-hide-medium w3-hide-large w3-right" href="javascript:void(0)" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
-    <a href="#" class="w3-bar-item w3-button w3-padding-large">HOME</a>
-    <a href="#contact" class="w3-bar-item w3-button w3-padding-large w3-hide-small">IDEA</a>
+    <a href="/index.php" class="w3-bar-item w3-button w3-padding-large">HOME</a>
+    <a href="/idea.php" class="w3-bar-item w3-button w3-padding-large w3-hide-small">IDEA</a>
  <a href="#contact" class="w3-bar-item w3-button w3-padding-large w3-hide-small">CONTACT</a>
               </div>
     </div>
