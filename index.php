d28e985d4bcbe58be8b7be0cc37334563c332493
convoluted <duminydaniel@gmail.com>
2018-02-28 21:07:54 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,119 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body, html {
+  height: 100%;
+  margin: 0;
+  font: 400 15px/1.8 "Lato", sans-serif;
+  color: #777;
+}
+
+.bgimg-1, .bgimg-2, .bgimg-3 {
+  position: relative;
+  opacity: 0.65;
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+
+}
+.bgimg-1 {
+  background-image: url("https://i2.wp.com/thewire.in/wp-content/uploads/2016/06/15686455816_bacea3fb20_o.jpg");
+  min-height: 100%;
+}
+
+.bgimg-2 {
+  background-image: url("http://www.globalharvestinitiative.org/wp-content/uploads/2017/11/Indian-farmer-plow-photo.jpg");
+  min-height: 400px;
+}
+
+.bgimg-3 {
+  background-image: url("http://cdn.publishyourarticles.net/wp-content/uploads/2015/06/india-women-tilling-earth.jpg");
+  min-height: 400px;
+}
+
+.caption {
+  position: absolute;
+  left: 0;
+  top: 50%;
+  width: 100%;
+  text-align: center;
+  color: #000;
+}
+
+.caption span.border {
+  background-color: #111;
+  color: #fff;
+  padding: 18px;
+  font-size: 25px;
+  letter-spacing: 10px;
+}
+
+h3 {
+  letter-spacing: 5px;
+  text-transform: uppercase;
+  font: 20px "Lato", sans-serif;
+  color: #111;
+}
+
+/* Turn off parallax scrolling for tablets and phones */
+@media only screen and (max-device-width: 1024px) {
+    .bgimg-1, .bgimg-2, .bgimg-3 {
+        background-attachment: scroll;
+    }
+}
+</style>
+</head>
+<body>
+
+<div class="bgimg-1">
+  <div class="caption">
+    <span class="border"> Digital Agriculture</span>
+  </div>
+</div>
+
+<div style="color: #777;background-color:white;text-align:center;padding:30px 90px;text-align: justify;">
+  <h3 style="text-align:center;">Proposing a New System</h3>
+  <p></br> <li>It's plain and simple. Digiagro lets every individual do agriculture from home. </li></br>
+ <li>It lets you invest and reap the benefits over your smartphone phone.</li></br>
+<li>It breaks the greatest barriers in agriculture - Investment, Location and Risks. </br>
+ <li>Imagine if a billion-citizen nation is given access to agriculture regardless of the place, job or money.</br>
+ </p>
+</div>
+
+<div class="bgimg-2">
+  <div class="caption">
+    <span class="border" style="background-color:transparent;font-size:25px;color: #f7f7f7;">
+ The first step towards the powerful economy</span>
+  </div>
+</div>
+
+<div style="position:relative;">
+  <div style="color:#ddd;background-color:#282E34;text-align:center;font-size:20px;padding:5px 20px;text-align: justify;">
+    <p>The very technology that drifted us away from agriculture will now help us rebound and revive</p>
+  </div>
+</div>
+
+<div class="bgimg-3">
+  <div class="caption">
+    <span class="border" style="background-color:transparent;font-size:25px;color: #f7f7f7;">SCROLL UP</span>
+  </div>
+</div>
+
+<div style="position:relative;">
+  <div style="color:#ddd;background-color:#282E34;text-align:center;font-size:20px;padding:10px 20px;text-align: justify;">
+    <p> <em>More formal waged labours who could be sure of the growth of their family and that of the nation<em></p>
+  </div>
+</div>
+
+<div class="bgimg-1">
+  <div class="caption">
+    <span class="border" style="color:blue" <a href=""> <a href=""> CLICK TO KNOW </a></span>
+  </div>
+</div>
+
+</body>
+</html>
